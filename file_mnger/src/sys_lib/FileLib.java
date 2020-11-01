189e45be44d740c32b33f389732cb78c235defc1
Keith <dangkhoa240899@gmail.com>
2020-11-01 20:06:55 +0700

    Methods for new file syscalls

--- file_mnger/src/sys_lib/FileLib.java
+++ file_mnger/src/sys_lib/FileLib.java
@@ -30,14 +30,71 @@ public class FileLib {
       e.printStackTrace();
     }
     finally {
-      if (fd != -1) fl.closeFile(fd);
+      if (fd >= 0) fl.closeFile(fd);
       else System.out.println("Error opening file - Error code " + fd);
+    }
+
+    // open/read/close test
+    try {
+      System.out.println("Opening file named " + fileName);
+      fd = fl.openFile(fileName);
+      String content = fl.readFile(fd);
+      System.out.println("Last system error: " + Native.getLastError());
+
+      System.out.println("\nFile content:\n" + content);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    finally {
+      if (fd >= 0) fl.closeFile(fd);
+      else System.out.println("Error opening file - Error code " + fd);
+    }
+
+    // mkdir/move/rename/unlink/rmdir test
+    try {
+      System.out.print("Enter your new directory's name: ");
+      Scanner sc = new Scanner(System.in);
+      String dirName = sc.nextLine();
+
+      System.out.println("Creating directory named " + dirName);
+      fl.createDir(dirName);
+      System.out.println(" ... Done.");
+      System.out.println("Listing files ...");
+      fl.listFiles();
+
+      String fileNewLoc = dirName + "/moved" + fileName;
+      fl.moveFile(fileName, fileNewLoc);
+      System.out.println("Last system error: " + Native.getLastError());
+      System.out.println("\nListing files ...");
+      fl.listFiles();
+
+      System.out.print("Deleting file named " + fileNewLoc);
+      fl.deleteFile(fileNewLoc);
+      System.out.println(" ... Done.");
+
+      System.out.println("Deleting directory named " + dirName);
+      fl.deleteDir(dirName);
+      System.out.println(" ... Done.");
+      System.out.println("\nListing files ...");
+      fl.listFiles();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
     }
   }
 
   private CStdLib clib;
   private Runtime rt;
   private final String psudohome = "/home/keith/Documents/psdhome/";
+  private final int __NR_open_file = 436;
+  private final int __NR_close_file = 437;
+  private final int __NR_read_file = 438;
+  private final int __NR_write_file = 439;
+  private final int __NR_reloc_file = 440;
+  private final int __NR_delete_file = 441;
+  private final int __NR_create_dir = 442;
+  private final int __NR_delete_dir = 443;
 
   public FileLib() throws Exception {
     clib = (CStdLib) Native.loadLibrary("c", CStdLib.class);
@@ -76,13 +133,74 @@ public class FileLib {
   }
 
   public long openFile(String fileName) throws Exception {
-    long fd = clib.syscall(2, psudohome + fileName, 66, 484);
+    long fd = clib.syscall(__NR_open_file, psudohome + fileName);
     this.printKernelLog();
     return fd;
   }
 
   public void closeFile(long fd) throws Exception {
-    clib.syscall(3, fd); 
+    clib.syscall(__NR_close_file, fd); 
+    this.printKernelLog();
+  }
+
+  public String readFile(long fd) throws Exception {
+    StringBuilder cont = new StringBuilder();
+    int chunkSize = 1000;
+    Pointer memBlock = new Memory(chunkSize);
+
+    long readCount = 0;
+    // System.out.println("[DEBUG] before #read_file#");//////////////
+    while (
+        (readCount = clib.syscall(__NR_read_file, fd, memBlock, chunkSize - 1)) > 0
+    ) {
+      // System.out.println("[DEBUG] right after #read_file#");//////////////
+      cont.append(memBlock.getString(0));
+      // System.out.println("[DEBUG] after receiving a chunk");//////////////
+      memBlock.clear(chunkSize);
+      // System.out.println("[DEBUG] after clearing the buffer");//////////////
+
+      this.printKernelLog();
+    }
+
+    return cont.toString();
+  }
+
+  public void writeFile(long fd, String cont) throws Exception {
+    clib.syscall(__NR_write_file, fd, cont, cont.length()); 
+    this.printKernelLog();
+  }
+
+  public void appendFile(long fd, String cont) throws Exception {
+    String existedCont = this.readFile(fd);
+    String updatedCont = existedCont + "\n" + cont;
+    this.writeFile(fd, updatedCont);
+  }
+
+  public void moveFile(String currentLoc, String newLoc) throws Exception {
+    // additional param `fname`?
+    clib.syscall(__NR_reloc_file, psudohome + currentLoc, psudohome + newLoc);
+    this.printKernelLog();
+  }
+
+  public void renameFile(String oldName, String newName) throws Exception {
+    // change params to (path, oldname, newname)?
+    clib.syscall(__NR_reloc_file, psudohome + oldName, psudohome + newName);
+    this.printKernelLog();
+  }
+
+  public void deleteFile(String fileName) throws Exception {
+    clib.syscall(__NR_delete_file, psudohome + fileName);
+    this.printKernelLog();
+  }
+
+  public void createDir(String dirName) throws Exception {
+    clib.syscall(__NR_create_dir, psudohome + dirName);
+    this.printKernelLog();
+  }
+
+  public void deleteDir(String dirName) throws Exception {
+    // delete the dir's contents ?
+    clib.syscall(__NR_delete_dir, psudohome + dirName);
     this.printKernelLog();
   }
 }
